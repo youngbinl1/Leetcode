8c407e8a51714d0adb5fe9e213e49b0e8ff94203
Youngbin Lee <104573477+youngbinl1@users.noreply.github.com>
2025-04-20 13:58:22 +1000

    Create 550_Game Play Analysis IV.sql

---
+++ Database/550_Game Play Analysis IV.sql
@@ -0,0 +1,3 @@
+select round(count(distinct player_id) / (select count(distinct player_id) from Activity), 2) as fraction
+from Activity 
+where (player_id, date_sub(event_date, interval 1 day)) IN (select player_id, min(event_date) from Activity group by player_id)
